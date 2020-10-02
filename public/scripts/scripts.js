7060d0cb2d7456d78cc445651dbc1ea2e3db2ab9
Zin <bookcase201@gmail.com>
2020-10-01 20:41:45 -0400

    aside from earlier updates also removed addexp script and combined with existing scripts.js

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -26,3 +26,48 @@ function confirmAddWorkedHere () {
 function confirmInterviewHere () {
     prompt("Do you want to add yourself to the list of people who have interviewed at this organization?\n \nTell us briefly about your experience being interviewed here.", " hiring process | interviewing questions | whiteboard challenge | etc.");
 }
+
+/**addexperience starts here */
+let addneworgbutton = document.getElementById('add-new-org');
+addneworgbutton.addEventListener('click', function (){
+    mynewOrg();
+});
+
+function mynewOrg(){
+    let x = document.getElementById('exp-current-organization');
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+let addpreviousorgbutton = document.getElementById('add-previous-org');
+addpreviousorgbutton.addEventListener('click', function (){
+    mypreviousOrg();
+});
+
+function mypreviousOrg(){
+    let x = document.getElementById('exp-previous-organization');
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+let addnewinterviewbutton = document.getElementById('add-new-interview');
+addnewinterviewbutton.addEventListener('click', function (){
+    mynewInterview();
+});
+
+function mynewInterview(){
+    let x = document.getElementById('add-interview');
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+/**addexperience ends here */
+
