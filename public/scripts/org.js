944f724b134147c8f6c58f905435c7ae906bcc06
Lois Simpson <lsimpson667@gmail.com>
2020-10-15 10:42:58 -0400

    added org.js file

---
+++ public/scripts/org.js
@@ -0,0 +1,42 @@
+export const ORG = [
+  {
+    id: 01,
+    name: "athenahealth",
+    logo: "http://localhost:5000/images/logo-athenahealth.png"
+  },
+  {
+    id: 02,
+    name: "CyberGrants",
+    logo: "http://localhost:5000/images/logo-CyberGrants.png"
+  },
+  {
+    id: 03,
+    name: "eXalt",
+    logo: "http://localhost:5000/images/logo-exaltsolutions.gif"
+  },
+  {
+    id: 04,
+    name: "Harvard University",
+    logo: "http://localhost:5000/images/logo-harvard.png"
+  },
+  {
+    id: 05,
+    name: "Liberty Mutual",
+    logo: "http://localhost:5000/images/logo-liberty-mutual.png"
+  },
+  {
+    id: 06,
+    name: "Rapid7",
+    logo: "http://localhost:5000/images/logo-rapid7.png"
+  },
+  {
+    id: 07,
+    name: "Tripadvisor",
+    logo: "http://localhost:5000/images/logo-tripadvisor.png"
+  },
+  {
+    id: 08,
+    name: "Wayfair",
+    logo: "http://localhost:5000/images/logo-Wayfair.jpg"
+  }
+]
