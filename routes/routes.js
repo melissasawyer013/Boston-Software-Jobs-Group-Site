0c0bca5e04367d9a09ec1af21642b4632b6c1da2
melissasawyer013 <melissasawyer013@gmail.com>
2021-01-16 00:56:42 -0500

    cleans up routes.js file to remove unneeded console.logs and axios

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,6 @@ let client = require('../db');
 const dotenv = require('dotenv');
 const db = require('../db');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 let token = null;
 const GitHubStrategy = require('passport-github2').Strategy;
 const passport = require('passport');
@@ -22,8 +21,6 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const SESSION_SECRET = process.env.SESSION_SECRET;
 
-
-
 let userEmail;
 let user;
 let githubUrl;
@@ -36,8 +33,6 @@ router.post('/checkEmail', (req, res) => {
   gradsFromDB.find({"email":userEmail}).toArray((err, arrayOfMatches) => {
     user = arrayOfMatches[0];
     githubUrl = user.githubUrl;
-    console.log (`The user object is ${JSON.stringify(user)}`);
-    console.log(`Their GitHub url is ${githubUrl}`);
     let match = arrayOfMatches[0].email;
     if (match === userEmail) {
       res.redirect('/login-after-email');
@@ -48,9 +43,6 @@ router.post('/checkEmail', (req, res) => {
   });  
 });
 
-
-
-// router.set('trust proxy', 1); // trust first proxy
 router.use(session({
   secret: SESSION_SECRET,
   resave: false,
@@ -70,18 +62,8 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function (user, done) {
-  // user=undefined;
-  // githubUrl=undefined;
-  // userEmail=undefined;
   done(null, user);
 })
-
-// passport.deserializeUser(function (user, cb) {
-//   user=undefined;
-//   githubUrl=undefined;
-//   userEmail=undefined;
-//   cb(null, user);
-// })
 
 passport.use(new GitHubStrategy({
   clientID: CLIENT_ID,
@@ -90,27 +72,12 @@ passport.use(new GitHubStrategy({
   callbackURL: "http://localhost:5000/auth/github/callback"
 },
 function(accessToken, refreshToken, profile, done) {
-  // console.log(`The profile from github is: ${profile}`);
-  console.log(`Access token: ${accessToken}`);
-  console.log(`Refresh token: ${refreshToken}`);
-  console.log(`The profile is: ${JSON.stringify(profile)}`);
   profileUrl = profile.profileUrl;
-  console.log(`The url from the authroizer is: ${profile.profileUrl}`);
-  console.log("Now I'm heading to the /auth/github/callback")
   return done(null, profile, accessToken, refreshToken);
-  // if (profile.profileUrl === githubUrl) {
-  //   console.log("THEY'RE A MATCH JOHNNY!");
-  //   res.redirect('/authorized')
-    
-  // } else {
-  //   res.redirect('/access-denied')
-  // }
-  
 }
 ));
 
-
-
+//NOT SURE IF WE EVEN NEED THIS
 const isAuth = (req, res, next, done) => {
   if (req.user) {
     console.log(`${user.firstName} is authenticated`)
@@ -120,21 +87,6 @@ const isAuth = (req, res, next, done) => {
     return done()
   }
 };
-
-// passport.use(new GitHubStrategy({
-//   clientID: CLIENT_ID,
-//   clientSecret: CLIENT_SECRET,
-//   callbackURL: "http://localhost:5000/auth/github/callback"
-// },
-// function(accessToken, refreshToken, profile, done) {
-//   User.findOrCreate({ githubId: profile.id }, function (err, user) {
-//     return done(err, user);
-//   });
-// }
-// ));
-
-
-
 
 router.get('/login', isAuth, (req, res) => {
   if (user) {
@@ -163,35 +115,20 @@ router.get('/login-after-email', (req, res) => {
   res.redirect(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`);
 });
 
-//https://github.com/login?client_id=044b8661035f08c4bb41&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3D044b8661035f08c4bb41
-
-
 router.get('/auth/github',
   passport.authenticate('github', {scope: [ 'user:user.html_url' ]
   })
 );
 
-
 router.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
     req.user;
-    console.log("I made it to the /auth/github/callback function");
-    console.log(`The githubUrl from the database is ${githubUrl} and the profileURL from the user is ${profileUrl}`);
-    // req.user.html_url;
-    // console.log(`html_url: ${user.html_url}`);
-    // Successful authentication, redirect to authorized page.
-    // console.log(user);
     if (githubUrl === profileUrl) {
-      console.log('The urls from the user and the database match!');
-      console.log(JSON.stringify(user));
       res.redirect('/authorized');
     } else {
-      console.log("The urls from the user and the database don't match");
-      // req.session.destroy();
       res.redirect('/access-denied');
-    }
-    
+    };
   }
 );
 
@@ -241,11 +178,8 @@ router.get('/graduates', (req, res) => {
   if (user) {
     let gradsFromDB = client.db(DB_NAME).collection(DB_GRAD);
     gradsFromDB.find({"year":2018}).sort({"lastName":1}).toArray((err, arrayOfGradsFromDb2018) => {
-      // arrayOfGradsFromDb2018.sort();
       gradsFromDB.find({"year":2019}).sort({"lastName":1}).toArray((err, arrayOfGradsFromDb2019) => {
-        // arrayOfGradsFromDb2019.sort();
         gradsFromDB.find({"year":2020}).sort({"lastName":1}).toArray((err, arrayOfGradsFromDb2020) => {
-          // arrayOfGradsFromDb2020.sort();
           res.render('pages/graduates', {
             user: user,
             grads2018: arrayOfGradsFromDb2018,
@@ -259,10 +193,6 @@ router.get('/graduates', (req, res) => {
     res.render('pages/login');
   };
 });
-
-// router.get('/login', (req, res) => {
-//   res.render('pages/login')
-// })
 
 router.get('/organizations', (req, res) => {
   if (user) {
@@ -281,54 +211,29 @@ router.get('/add-org', (req, res) => {
   res.render('pages/add-org')
 })
 
-// working kind of 
-// router.post('/checkEmail', (req, res) => {
-//   let formData = req.body;
-//   let userEmail = formData['emailAddress'];
-//   console.log(userEmail);
-//   let gradsFromDB = client.db(DB_NAME).collection(DB_GRAD);
-//   let searchResults = gradsFromDB.find({"email":userEmail});
-//   console.log(gradsFromDB.find({"email":userEmail}));
-//   if (searchResults != undefined) {
-//     console.log(searchResults);
-//     console.log("I think it was successful");
-//     res.redirect('/');
-//   } else {
-//     res.redirect('/login')
-//   }
-// });
-
 router.post('/add-org', (req, res) => {
   const form_data =req.body;
   console.log(form_data);
-
   const orgName = form_data['name'];
   const orgUrl = form_data['url'];
   const orgLogo = form_data['logo'];
-
   console.log(orgName, orgUrl, orgLogo);
-
   const org_obj = {
     name: orgName,
     url: orgUrl,
     logo: orgLogo
   }
   console.log(org_obj);
-  
-
   let orgsFromDB =client.db(DB_NAME).collection(DB_ORG)
   orgsFromDB.insertOne(org_obj, (error, result) => {
   if (error) {
     console.log(error);
   } else {
     console.log("AN ORG ENTRY HAS BEEN ADDED")
-
     res.redirect('/add-org');
   }
-    
   })
 })
-
 
 router.get('/style-guide', (req, res) => {
   res.render('pages/style-guide')
@@ -339,10 +244,6 @@ router.get('/graduate-card', (req, res) => {
 })
 
 router.get('/organization-card', (req, res) => {
-  // db_handler.collection(DB_ORG).find({}).toArray((err, org) => {
-  //     if(err) return console.log(err);
-  //     if(org) res.render('pages/organization-card', {orgArray:org})
-  // });
   res.render('pages/organization-card');
 })
 
